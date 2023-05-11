ae70ab5c364e6098de51fd9bec319e39c0035c7a
Evan M <evanmwastaken@gmail.com>
2023-05-11 18:41:13 -0400

    Names of ID's fixed

--- app.js
+++ app.js
@@ -8,15 +8,18 @@ form.addEventListener('submit', (event) => {
   
   // Get the input values from the form
   const name = document.querySelector('#name').value.trim();
-  const email = document.querySelector('#email').value.trim();
-  const phone = document.querySelector('#phone').value.trim();
-  const isAttending = document.querySelector('#isAttending').checked;
-  const isVolunteering = document.querySelector('#isVolunteering').checked;
-  const hasSpecialNeeds = document.querySelector('#hasSpecialNeeds').checked;
-  const dietaryRestrictions = document.querySelector('#dietaryRestrictions').value.trim();
+  const email = document.querySelector('#TeamNumber').value.trim();
+  const TeamName = document.querySelector('#TeamName').value.trim();
+  const Weight = document.querySelector('#Weight').value.trim();
+  const dimensions = document.querySelector('#dimensions').value.trim();
+  const HasSwerve = document.querySelector('#HasSwerve').checked;
+  const HiLowGears = document.querySelector('#HiLowGears').checked;
+  const Limelight = document.querySelector('#limelight').checked;
+  const AprilTags = document.querySelector('#AprilTags').checked;
+  const comments = document.querySelector('#comments').value.trim();
   
   // Generate the data string to be encoded in the QR code
-  const dataString = `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nAttending: ${isAttending}\nVolunteering: ${isVolunteering}\nSpecial Needs: ${hasSpecialNeeds}\nDietary Restrictions: ${dietaryRestrictions}`;
+  const dataString = `name: ${name}\nTeamNumber: ${TeamNumber}\nTeamName: ${TeamName}Weight: ${Weight}\ndimensions: ${dimensions}\nHasSwerve: ${HasSwerve}\nHiLowGears: ${HiLowGears}\nlimelight: ${limelight}\nAprilTags: ${AprilTags}\ncomments: ${comments}`;
   
   // Generate the QR code using the QRCode.js library
   const qrcode = new QRCode(qrCodeDiv, {
